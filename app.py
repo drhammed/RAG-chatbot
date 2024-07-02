a719e332eb973ce756268dfc9a82b18f32acf416
Hammed Akande <48578759+drhammed@users.noreply.github.com>
2024-07-01 20:25:56 -0400

    update

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
-#pip install langchain_voyageai
-#!pip install langchain_openai
-#!pip install langchain_pinecone
-#pip install groq
-#!pip install langchain_groq
-
-
 import streamlit as st
 from langchain_voyageai import VoyageAIEmbeddings
 import os
 import json
 import boto3
-from dotenv import load_dotenv
 from urllib.parse import urlparse
 from pinecone import Pinecone
 import pinecone
 from langchain_openai import ChatOpenAI
 import openai
 from groq import Groq
-from langchain.chains import LLMChain, RetrievalQA
+from langchain.chains import LLMChain
 import time
 import re
 import warnings
 from langchain_pinecone import PineconeVectorStore
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferWindowMemory
 from langchain.schema import HumanMessage
 from langchain.prompts import ChatPromptTemplate
-from langchain.chains import ConversationChain
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.runnables.base import Runnable
@@ -35,34 +26,27 @@ from langchain_core.prompts import (
     MessagesPlaceholder,
 )
 from langchain_core.messages import SystemMessage
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain_groq import ChatGroq
 import uuid
 
-# Load environment variables from .env file
-#load_dotenv()
+# Rate limiting parameters
+REQUEST_LIMIT = 6000  # The token limit per minute
+REQUEST_INTERVAL = 60  # Time interval in seconds
+last_request_time = 0  # Time of the last request
 
-# Initialize Pinecone
-#pc = Pinecone(api_key=os.getenv("My_Pinecone_API_key"))
-## HARD CODED index names and host
-#index_name = "diabetes-ind"
-#index = pc.Index(index_name, host="https://diabetes-ind-3w8l5y1.svc.aped-4627-b74a.pinecone.io")
+def rate_limited_request(query, conversation):
+    global last_request_time
+    current_time = time.time()
+    time_since_last_request = current_time - last_request_time
 
-# Initialize VoyageAI
-#voyageai.api_key = os.getenv("My_voyageai_API_key")
-#vo = voyageai.Client()
+    if time_since_last_request < REQUEST_INTERVAL:
+        wait_time = REQUEST_INTERVAL - time_since_last_request
+        st.warning(f"Rate limit reached. Waiting for {wait_time:.2f} seconds...")
+        time.sleep(wait_time)
 
-# Initialize OpenAI
-#openai_api_key = os.getenv('My_OpenAI_API_key')
-#openai.api_key = openai_api_key
-
-
-# Ignore all warnings
-warnings.filterwarnings("ignore")
-
-# Set up Streamlit app
-st.set_page_config(page_title="Custom Chatbot", layout="wide")
-st.title("Custom Chatbot with Retrieval Abilities")
+    response = conversation.predict(human_input=query)
+    last_request_time = time.time()
+    return response
 
 # Function to generate pre-signed URL
 def generate_presigned_url(s3_uri):
@@ -105,31 +89,6 @@ def retrieve_and_format_response(query, retriever, llm):
     response = llm([message])
     return response
 
-# Function to save chat history to a file
-def save_chat_history_to_file(filename, history):
-    with open(filename, 'w') as file:
-        file.write(history)
-
-# Function to upload the file to S3
-def upload_file_to_s3(bucket, key, filename):
-    s3_client.upload_file(filename, bucket, key)
-
-# Example usage with memory
-def ask_question(query, chain, llm):
-    # Retrieve and format the response with pre-signed URLs
-    response_with_docs = retrieve_and_format_response(query, retriever, llm)
-    
-    # Add the retrieved response to the memory
-    memory.save_context({"input": query}, {"output": response_with_docs['answer']})
-    
-    # Use the conversation chain to get the final response
-    response = chain.invoke(query)
-    pattern = r"s3(.*?)(?=json)"
-    s3_uris = ["s3" + x + "json" for x in re.findall(pattern, response)]
-    for s3_uri in s3_uris:
-        final_response = response.replace(s3_uri, generate_presigned_url(s3_uri))
-    return final_response
-
 # Setup - Streamlit secrets
 OPENAI_API_KEY = st.secrets["api_keys"]["OPENAI_API_KEY"]
 VOYAGE_AI_API_KEY = st.secrets["api_keys"]["VOYAGE_AI_API_KEY"]
@@ -138,29 +97,24 @@ GROQ_API_KEY = st.secrets["api_keys"]["GROQ_API_KEY"]
 aws_access_key_id = st.secrets["aws"]["aws_access_key_id"]
 aws_secret_access_key = st.secrets["aws"]["aws_secret_access_key"]
 aws_region = st.secrets["aws"]["aws_region"]
-    
 
-# Langchain stuff
-#llm = ChatOpenAI(model="gpt-4o", openai_api_key=OPENAI_API_KEY)
-
-#Groq model
-model = 'llama3-70b-8192'
 # Initialize Groq Langchain chat object and conversation
+model = 'llama3-70b-8192'
 llm = ChatGroq(groq_api_key=GROQ_API_KEY, model_name=model, temperature=0.02)
 
-
-# Initialize the conversation memory
-memory = ConversationBufferMemory()
+# Initialize the conversation memory with a window length of 5
+conversational_memory_length = 5
+memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
 
 prompt_template = ChatPromptTemplate.from_template(
-        "Instruction: You are a helpful assistant to help users with their patient education queries. \
-        Based on the following information, provide a summarized & concise explanation using a couple of sentences. \
-        Only respond with the information relevant to the user query {query}, \
-        if there are none, make sure you say the `magic words`: 'I don't know, I did not find the relevant data in the knowledge base.' \
-        But you could carry out some conversations with the user to make them feel welcomed and comfortable, in that case you don't have to say the `magic words`. \
-        In the event that there's relevant info, make sure to attach the download button at the very end: \n\n[More Info]({s3_gen_url}) \
-        Context: {combined_content}"
-    )
+    "Instruction: You are a helpful assistant to help users with their patient education queries. \
+    Based on the following information, provide a summarized & concise explanation using a couple of sentences. \
+    Only respond with the information relevant to the user query {query}, \
+    if there are none, make sure you say the `magic words`: 'I don't know, I did not find the relevant data in the knowledge base.' \
+    But you could carry out some conversations with the user to make them feel welcomed and comfortable, in that case you don't have to say the `magic words`. \
+    In the event that there's relevant info, make sure to attach the download button at the very end: \n\n[More Info]({s3_gen_url}) \
+    Context: {combined_content}"
+)
 
 # Initialize necessary objects (s3 client, Pinecone, OpenAI, etc.)
 s3_client = boto3.client(
@@ -171,31 +125,25 @@ s3_client = boto3.client(
 )
 
 # Initialize Pinecone
-#pc = Pinecone(api_key=os.getenv("My_Pinecone_API_key"))
 os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
 pc = Pinecone(api_key=PINECONE_API_KEY)
-## HARD CODED index names and host
 index_name = "diabetes-ind"
-#index = pc.Index(index_name, host="https://diabetes-ind-3w8l5y1.svc.aped-4627-b74a.pinecone.io")
 
 # Initialize OpenAI
 openai.api_key = OPENAI_API_KEY
 
 # Set up LangChain objects
-# VOYAGE AI
 model_name = "voyage-large-2"  
 embedding_function = VoyageAIEmbeddings(
     model=model_name,  
     voyage_api_key=VOYAGE_AI_API_KEY
 )
-# Initialize the Pinecone client
 vector_store = PineconeVectorStore.from_existing_index(
     embedding=embedding_function,
     index_name=index_name
 )
 retriever = vector_store.as_retriever()
 
-# Initialize rag_chain
 rag_chain = (
     {"retrieved_context": retriever, "question": RunnablePassthrough()}
     | prompt_template
@@ -224,7 +172,7 @@ if user_input:
     
     # Generate and display bot response
     with st.spinner("Thinking..."):
-        bot_response = retrieve_and_format_response(user_input, retriever, llm).content
+        bot_response = rate_limited_request(user_input, rag_chain)
     
     st.session_state["messages"].append({"role": "assistant", "content": bot_response})
     
