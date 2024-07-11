3167e919d214f9e46a4f3a80deca6bb41c65acdf
Hammed Akande <48578759+drhammed@users.noreply.github.com>
2024-07-10 20:56:03 -0400

    upd

--- app.py
+++ app.py
@@ -222,4 +222,6 @@ def chat():
         })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port, debug=True)
+
